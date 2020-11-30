f080ca245cb4e90d6610f83e66e5844e85ce5c77
fukusuke1026 <fukusuke.takahashi@gmail.com>
2020-11-30 22:48:16 +0900

    add jest tutorial

---
+++ app.test.js
@@ -0,0 +1,52 @@
+const { test, expect } = require('@jest/globals');
+const sum = require('./app');
+
+test("A", () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test("B", () => {
+    expect(2 + 2).toBe(4);
+});
+
+test("C", () => {
+    const data = { one: 1 };
+    data['two'] = 2;
+    expect(data).toEqual({ one: 1, two: 2 })
+});
+
+test("null", () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+});
+
+test("plus", () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+});
+
+test('m', () => {
+    expect("team").not.toMatch(/I/);
+});
+
+test("m2", () => {
+    expect("christoph").toMatch(/stop/);
+});
+
+const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'beer',
+];
+
+test("l", () => {
+    expect(shoppingList).toContain('beer');
+    expect(new Set(shoppingList)).toContain('beer');
+})
+
+test("k", () => {
+    expect(() => {throw new Error("")}).toThrow();
+})
