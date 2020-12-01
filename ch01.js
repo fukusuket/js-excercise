9e7e0f9f1371bc2ba617cd12a1e077c8d819be2d
fukusuke1026 <fukusuke.takahashi@gmail.com>
2020-12-01 23:43:34 +0900

    Mastering-JavaScript-Functional-Programming-2nd-Edition. ch1 study.

---
+++ ch01.js
@@ -0,0 +1,13 @@
+const fact2 = n => {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * fact2(n - 1);
+    }
+}
+
+const fact3 = n => (n === 0 ? 1 : n * fact3(n-1))
+
+const sum3 = (a,b,c) =>  a + b + c
+
+module.exports = {fact2, fact3, sum3}
